f7a586490a254ea1067e9533c85e785b5b6f7ff7
dang.vn <dang@gmail.com>
2024-04-06 09:10:59 +0700

    Alert

--- app/src/main/java/com/nlhd/xaydungapptest/MainActivity.java
+++ app/src/main/java/com/nlhd/xaydungapptest/MainActivity.java
@@ -1,37 +1,62 @@
 package com.nlhd.xaydungapptest;
 
-import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.MenuItem;
+
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.Gallery;
-import android.widget.ImageView;
-import android.widget.ViewAnimator;
-import android.widget.ViewSwitcher;
+import android.widget.AdapterViewFlipper;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
-
-
+    boolean kt = false;
 
     @SuppressLint("MissingInflatedId")
-    @Override
+    @Override   
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        //b comment
+    }
 
+    @Override
+    public void onBackPressed() {
+        if(kt == false)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Notification");
+            builder.setMessage("Do you want to exit");
+            builder.setCancelable(false);
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            builder.show();
+            kt = true;
+        }
+        else
+        {
+            kt = false;
+            super.onBackPressed();
+        }
     }
 }
